085c740a388caf8dcd7897c611b6f790c8114d26
Vitor Oliveira <104097532+OliveiraHackerSecurity@users.noreply.github.com>
2022-05-11 11:11:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from googlesearch import search
 
 # Texto de busca
-busca = int(input('Digite a palavra de busca: '))
+busca = 'Digite a palavra de busca'
 
 # Faz uma busca e cria lista com os link's do resultado
 resultado = list(
        search(
            busca,
            lang='pt-br',
-           num_results=5
+           num_results=8
        )
 )
 
